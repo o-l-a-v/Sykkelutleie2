c29bddf3315796fa9585faa80582459f527b756c
SgtBake <henrykh@student.hist.no>
2015-03-23 10:45:07 +0100

    sync1

--- Sykkelutleie/main.vb
+++ Sykkelutleie/main.vb
@@ -6,5 +6,19 @@
             Me.Hide()
         End If
     End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        finnKunde.Show()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        finnSykkel.Show()
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        finnUtstyr.Show()
+
+    End Sub
 End Class
 
